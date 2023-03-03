799e605f5162cb091474659b944ce2ad904d53ea
Reynaldo C. Depagon <55631616+depagon100@users.noreply.github.com>
2023-03-04 03:34:08 +0800

    Added Create Trainee in admin panel need to troubleshoot the save when registering a trainee rename button Create Trainee to Create Account

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                         <div class="m-auto flex justify-end p-2 ">
                             @if (Route::has('register'))
 
-                                    <a href="{{ route('create') }}" class="px-4 py-2 mb-6 text-white no-underline rounded-full transition ease-in-out delay-150 bg-indigo-400 hover:-translate-y-1 hover:scale-110 hover:bg-blue-500 duration-300">Create Trainee</a>
+                                    <a href="{{ route('create') }}" class="px-4 py-2 mb-6 text-white no-underline rounded-full transition ease-in-out delay-150 bg-indigo-400 hover:-translate-y-1 hover:scale-110 hover:bg-blue-500 duration-300">Create Account</a>
 
                             @endif
                         </div>
