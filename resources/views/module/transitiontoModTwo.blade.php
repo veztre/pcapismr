00dc1182dae903217c137988ec1712825b469ad4
Reynaldo C. Depagon <55631616+depagon100@users.noreply.github.com>
2023-03-04 03:04:51 +0800

    added Transition to Mod2 to view no db, controller and model setup

---
+++ resources/views/module/transitiontoModTwo.blade.php
@@ -0,0 +1,59 @@
+<x-app-layout>
+
+
+    <div class="py-12 ">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mb-6">
+
+                <!--Card-->
+                <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white m-auto ">
+
+                    <h1 class=" mr-4 font-sans font-bold break-normal text-indigo-500 px-2 py-2 text-xl md:text-2xl ">
+                        <p class="p-2 bg-secondary text-light">QUESTIONS BEFORE WE PROCEED TO MODULE 2</p>
+                    </h1>
+
+                    <!--Title-->
+
+                    <br>
+
+                    <p>Is the establishment an importer, distributor, manufacturer/producer, or user of any industrial
+                        chemicals under CCO?</p>
+
+                    <form>
+                        <label class="radio-inline mr-3">
+                            <input type="radio" name="optradio" class="mr-1" checked>Yes
+                        </label>
+                        <label class="radio-inline mr-3">
+                            <input type="radio" name="optradio" class="mr-1">No
+                        </label>
+                    </form>
+
+                    <br>
+
+                    <p>Is the establishment a hazardous waste Treater/Recycler?</p>
+
+                    <form>
+                        <label class="radio-inline mr-3">
+                            <input type="radio" name="optradio" class="mr-1" checked>Yes
+                        </label>
+                        <label class="radio-inline mr-3">
+                            <input type="radio" name="optradio" class="mr-1">No
+                        </label>
+                    </form>
+
+
+                    <br>
+
+
+
+                </div>
+            </div>
+            <div style="float: right" class="mb-3">
+                <a href="" class="btn btn-lg border bg-light">Previous</a>
+                <a href="" class="btn btn-lg btn-primary">Proceed to Module 2</a>
+            </div>
+        </div>
+
+    </div>
+
+</x-app-layout>
