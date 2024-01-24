e715ef645b03026c4c6ff13f180d3bfb60f076a8
Enrico P. Chavez <vez3@yahoo.com>
2024-01-24 21:55:30 +0800

    update db onnection

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'pcapi-smr-server.mysql.database.azure.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'pcapi-smr-db'),
+            'username' => env('DB_USERNAME', 'yqveuvghag'),
+            'password' => env('DB_PASSWORD', 'VU7434PS12YO02YF$'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA','/home/site/wwwroot/ssl/DigiCertGlobalRootCA.crt.pem'),
             ]) : [],
         ],
 
