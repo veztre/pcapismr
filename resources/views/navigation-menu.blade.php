5d02650e809597826d6b868f754f06d8b47e517b
Enrico P. Chavez <vez3@yahoo.com>
2024-08-11 18:53:52 +0800

    update the logout button

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -95,22 +95,20 @@
                         </svg> {{ __('Profile') }}
                         </x-jet-nav-link>
 
-                                <form method="POST" action="{{ route('logout') }}" x-data>
+                            <form method="POST" action="{{ route('logout') }}" x-data>
                                 @csrf
-                                <x-jet-nav-link href="{{ route('logout') }}" :active="request()->routeIs('dashboard')" class="text-blue-600 no-underline font-semibold text-base" @click.prevent="$root.submit();">
-                        <i class="bi bi-house"></i>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house mr-2"
-                             viewBox="0 0 16 16">
-                            <path fill-rule="evenodd"
-                                  d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
-                            <path fill-rule="evenodd"
-                                  d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
-                        </svg> {{ __('Logout') }}
-                        </x-jet-nav-link>
-
-
+                                    <a href="{{ route('logout') }}" class="text-blue-600 no-underline font-semibold text-base" @click.prevent="$root.submit();">
+                                    <i class="bi bi-house"></i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house mr-2"
+                                        viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd"
+                                            d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                                        <path fill-rule="evenodd"
+                                            d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+                                    </svg> {{ __('Logout') }}
+                                    </a>
                             </form>
-                            
+
 
 
 
