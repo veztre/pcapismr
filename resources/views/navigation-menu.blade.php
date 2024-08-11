387a6c516aac41649ec4e62e4077d7cfefe8af04
Enrico P. Chavez <vez3@yahoo.com>
2024-08-11 18:47:29 +0800

    updat the navigation menu expose the logout option

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -72,36 +72,48 @@
                         <div id="pst-time" class="m-auto mb-2 text-right sm:text-sm"></div>
                         <div><a href="https://gwhs.i.gov.ph/pst/" id="pst-source"  target="_blank"></a></div>
                     </div>
-                    <x-jet-dropdown width="48" class="position-relative sm:inline-block" >
-                        <x-slot name="trigger">
-                                <span class="inline-flex rounded-md">
-                                    <p>Logged in as:
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-lg leading-4 font-medium rounded-md text-blue-500 hover:text-blue-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
-                                        {{ Auth::user()->firstname }} {{Auth::user()->lastname}}</p>
 
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                        </svg>
-                                    </button>
-                                </span>
-                        </x-slot>
-                        <x-slot name="content"  >
-                            <!-- Account Management -->
-                            <x-jet-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
-                            </x-jet-dropdown-link>
-                            <div class="border-t border-gray-100 "></div>
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}" x-data>
+                        <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" class="text-blue-600 no-underline font-semibold text-base">
+                        <i class="bi bi-house"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house mr-2"
+                             viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                  d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                            <path fill-rule="evenodd"
+                                  d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+                        </svg> {{ Auth::user()->firstname }} {{Auth::user()->lastname}}
+                        </x-jet-nav-link>
+
+                        <x-jet-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('dashboard')" class="text-blue-600 no-underline font-semibold text-base">
+                        <i class="bi bi-house"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house mr-2"
+                             viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                  d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                            <path fill-rule="evenodd"
+                                  d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+                        </svg> {{ __('Profile') }}
+                        </x-jet-nav-link>
+
+                                <form method="POST" action="{{ route('logout') }}" x-data>
                                 @csrf
-                                <x-jet-dropdown-link href="{{ route('logout') }}"
-                                                     @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
-                                </x-jet-dropdown-link>
-                            </form>
+                                <x-jet-nav-link href="{{ route('logout') }}" :active="request()->routeIs('dashboard')" class="text-blue-600 no-underline font-semibold text-base" @click.prevent="$root.submit();">
+                        <i class="bi bi-house"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house mr-2"
+                             viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                  d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                            <path fill-rule="evenodd"
+                                  d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+                        </svg> {{ __('Logout') }}
+                        </x-jet-nav-link>
 
-                        </x-slot>
-                    </x-jet-dropdown>
+
+                            </form>
+                            
+
+
+
                 </div>
             </div>
 
